18d4123463c174ffc89ef66c177b5dc3c0a44890
Pavel Vasek <pvasek@gmail.com>
2014-10-22 21:37:50 +0200

    For the first two bytes the initial values for binary search are precalculated

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,7 @@
 var assert = require('assert');
-var utils = require('../src/utils');
+var Q = require('q');
+var parsingUtils = require('../src/utils/parsing_utils');
+var binaryList = require('../src/utils/binary_list');
 
 var start = process.hrtime();
 
@@ -10,11 +12,15 @@ var stopwatch = function(note){
     start = process.hrtime(); // reset the timer
 }
 
+var testFilePath = './data/test.csv';
+var testBigFilePath = './data/partial.csv';
+var testVectorFilePath = './data/test_vector.csv';
+
 describe('utils module', function(){
 
 	it('should read whole file', function(done){
 		this.timeout(5000);
-		var res = utils.parseFile('./../data/test.csv');
+		var res = parsingUtils.parseFile(testFilePath);
 		var data = [];
 		res
 			.then(function(){
@@ -27,20 +33,22 @@ describe('utils module', function(){
 			.catch(function(e) { done(e); });
 	});
 
+
+
 	it ('should parse ip', function(){
-		var ip = utils.parseIp("2037380272");
+		var ip = parsingUtils.parseIp("2037380272");
 		assert.deepEqual([121,111,244,176], ip);
 	});
 
 
 	describe('binary list', function(){
 
-		it('search direct match', function(done) {
-			var res = utils.parseFile('./../data/test.csv');
+		it('should find a direct match (low area)', function(done) {
+			var res = parsingUtils.parseFile(testFilePath);
 			var data = [];
 			res.then(function(){
 
-				var list = utils.createBinaryList(data);
+				var list = binaryList.createBinaryList(data);
 				var result = list.find(2001406464);
 				assert.equal("B", result.country);
 				done();
@@ -51,12 +59,28 @@ describe('utils module', function(){
 			.catch(function(e) { done(e); });
 		});
 
-		it('search in-direct match', function(done) {
-			var res = utils.parseFile('./../data/test.csv');
+		it('should find a direct match (high area)', function(done) {
+			var res = parsingUtils.parseFile(testFilePath);
 			var data = [];
 			res.then(function(){
 
-				var list = utils.createBinaryList(data);
+				var list = binaryList.createBinaryList(data);
+				var result = list.find(2001406478);
+				assert.equal("B", result.country);
+				done();
+			}, function(){
+			}, function(item) {
+				data.push(item);
+			})
+			.catch(function(e) { done(e); });
+		});
+
+		it('should find in-direct match', function(done) {
+			var res = parsingUtils.parseFile(testFilePath);
+			var data = [];
+			res.then(function(){
+
+				var list = binaryList.createBinaryList(data);
 				var result = list.find(2001406470);
 				assert.equal("B", result.country);
 				done();
@@ -66,30 +90,48 @@ describe('utils module', function(){
 			})
 			.catch(function(e) { done(e); });
 		});
+
+		it('should not found anything for missing ip', function(done) {
+			var res = parsingUtils.parseFile(testFilePath);
+			var data = [];
+			res.then(function(){
+
+				var list = binaryList.createBinaryList(data);
+				var result = list.find(2001406479);
+				assert.equal(null, result);
+				done();
+			}, function(){
+			}, function(item) {
+				data.push(item);
+			})
+			.catch(function(e) { done(e); });
+		});
 	});
 
-	it.skip('performence test', function(done){
-		var res = utils.parseFile('./../data/partial.csv');
+	it.only('performence test', function(done){
+		var dataPromise = parsingUtils.parseFile(testBigFilePath);
 		var data = [];
-		res.then(function(){
+		
+		dataPromise.then(function(){			
 
-			stopwatch('building list..');
-			var list = utils.createBinaryList(data);
-			stopwatch('find1');
+			stopwatch('starting');
+			var list = binaryList.createBinaryList(data, false);
+			stopwatch('building binary list');
 			var result = list.find(2002534209);
-			stopwatch('find2');
-			var result = list.find(2002534209);
-			stopwatch('end');
-			console.log('looking for: ' + 2002534209);
-			console.log('got:');
-			console.log(result);
+			stopwatch('warm up');
+			for (var i = 0; i < 1000000; i++) {
+				var result = list.find(2002534209);
+			}
+			stopwatch('test for 1.000.000 calls');
 			assert.equal("PHILIPPINES", result.country);
 			done();
+
 		}, function(){
 		}, function(item) {
 			data.push(item);
 		})
 		.catch(function(e) { done(e); });
+		
 	});
 
 
